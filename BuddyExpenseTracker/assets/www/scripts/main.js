e2dcd0acdc982ae78646f182710d399e61c7368b
ananyaapps <ananyaapps@gmail.com>
2011-10-09 00:59:49 +0530

    Revert "Added require.js to load scripts"
    This reverts commit 9111959f5b9dd63c736ba277908ed030b2df74be

--- BuddyExpenseTracker/assets/www/scripts/main.js
+++
@@ -1,4 +0,0 @@
-//Load the dependent javascripts
-require(["stdlib/jquery-1.6.2", "stdlib/phonegap-1.0.0", "mylib/buddy-libraries", "mylib/buddy-expense-tracker", "mylib/buddy-database", "stdlib/jquery.mobile"], function(someModule) {
-	alert('js loaded');
-});
